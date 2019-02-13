dffe3a5996500be1bcbd2155a2debf2e2b212f71
karansp1997 <karansp1997@gmail.com>
2019-02-13 12:49:33 +0530

    hello

---
+++ Practice_practice/src/practice_prog/Solution.java
@@ -0,0 +1,35 @@
+package practice_prog;
+
+import java.util.Scanner;
+
+public class Solution {
+	
+	
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int i = 0;
+		@SuppressWarnings("resource")
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		while(i <= N)
+		{
+			if(i %2==0)
+			{
+					System.out.println(i+"\n");
+			}
+		}
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
